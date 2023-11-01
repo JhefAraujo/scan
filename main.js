905b735bc90654e68c6c60d3b1898761e7a4f083
Jhefferson Araujo <jhefferson.araujo@gmail.com>
2023-11-01 16:07:13 -0300

    j

--- main.js
+++ main.js
@@ -1,56 +1,79 @@
-
+function close() {
+  document.getElementById("popup").style.pointerEvents = "none";
+  document.getElementById("popup").style.opacity = "0";
+}
 
 function scan() {
-    document.getElementById('load').style.display = 'flex';
-    var myHeaders = new Headers();
-    myHeaders.append("apikey", "helloworld");
+  document.getElementById("load").style.display = "flex";
+  var myHeaders = new Headers();
 
-    var formdata = new FormData();
-    formdata.append("language", "por");
-    formdata.append("isOverlayRequired", "false");
-    formdata.append("file", document.getElementById('file').files[0], "[PROXY]");
-    formdata.append("iscreatesearchablepdf", "false");
-    formdata.append("issearchablepdfhidetextlayer", "false");
-    formdata.append("OCREngine", "2");
-    formdata.append("filetype", "jpg");
+  myHeaders.append("apikey", "helloworld");
 
+  var formdata = new FormData();
+  formdata.append("language", "eng");
+  formdata.append("isOverlayRequired", "false");
+  formdata.append("file", document.getElementById("file").files[0], "[PROXY]");
+  formdata.append("iscreatesearchablepdf", "false");
+  formdata.append("issearchablepdfhidetextlayer", "false");
+  formdata.append("OCREngine", "2");
+  formdata.append("filetype", "jpg");
 
-    var requestOptions = {
-        method: 'POST',
-        headers: myHeaders,
-        body: formdata,
-        redirect: 'follow'
-    };
+  var requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: formdata,
+    redirect: "follow",
+  };
 
-    async function recieve() {
-        response = await fetch("https://api.ocr.space/parse/image", requestOptions)
-        jponse = response.json();
-        jwait = await jponse;
-        parsedResults = jwait["ParsedResults"];
-        zero = parsedResults[0];
-        overlays = zero["TextOverlay"];
-        lines = overlays.Lines;
+  async function recieve() {
+    response = await fetch("https://api.ocr.space/parse/image", requestOptions);
+    jponse = response.json();
+    jwait = await jponse;
+    parsedResults = jwait["ParsedResults"];
+    zero = parsedResults[0];
+    overlays = zero["TextOverlay"];
+    lines = overlays.Lines;
 
-        for (let i = 0; i < lines.length; i++) {
-            let line = lines[i].LineText;
+    for (let i = 0; i < lines.length; i++) {
+      let line = lines[i].LineText;
+      console.log(line);
 
-            let isnum = /^\d+$/.test(line);
+      let isnum = /^\d+$/.test(line);
 
-            if (line.length == 7 && isnum == true) {
-                cria = document.createElement('p');
-                cria.setAttribute('id', 'cria')
-                document.getElementById('form').appendChild(cria);
-                cria.innerHTML = line;
-                console.log(line);
-            }
-        }
-        document.getElementById('load').style.display = 'none';
+      if (line.length == 7 && isnum == true) {
+        cria = document.createElement("p");
+        cria.setAttribute("id", "cria");
+        document.getElementById("form").appendChild(cria);
+        cria.innerHTML = line;
+      }
     }
-    recieve();
+    document.getElementById("load").style.display = "none";
+  }
+  recieve();
 }
 
 async function send() {
-    valor = document.getElementById('cria').innerHTML;
-    document.getElementById('validate').value = valor;
-    document.getElementById('send').submit();
+  valor = document.getElementById("cria").innerHTML;
+
+  var formdata = new FormData();
+  formdata.append("validate", valor);
+
+  var requestOptions = {
+    method: "POST",
+    body: formdata,
+    redirect: "follow",
+  };
+
+  fetch(
+    "https://script.google.com/macros/s/AKfycbwFc3pdSHHsDys8MeCW4O3AT7vqCgJJBidGiE6hYtL4ie1hdge23IS1YNG0JXjbYYRB/exec",
+    requestOptions
+  )
+    .then((response) => response.text())
+    .then((result) => (document.getElementById("os").innerHTML = result))
+    .catch((error) => console.log("error", error));
+
+  if (document.getElementById("os").innerHTML != "") {
+    document.getElementById("popup").style.opacity = "1";
+    document.getElementById("popup").style.pointerEvents = "auto";
+  }
 }
